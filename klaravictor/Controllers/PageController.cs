4895482748db013183971b38676d0168fb640ca9
Joakim Wågström <joakim.wagstrom@knowit.se>
2018-09-16 21:55:40 +0200

    Rvsp form added

--- klaravictor/Controllers/PageController.cs
+++ klaravictor/Controllers/PageController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using klaravictor.Models;
 
 namespace klaravictor.Controllers
 {
@@ -14,9 +15,9 @@ namespace klaravictor.Controllers
         }
 
         [HttpPost]
-        public ActionResult Rvsp()
+        public ActionResult Index(RvspModel rvsp)
         {
-            return ViewBag("Posted");
+            return View();
         }
     }
 }
---
+++ klaravictor/Models/RvspModel.cs
@@ -0,0 +1,14 @@
+﻿namespace klaravictor.Models
+{
+    public class RvspModel
+    {
+        public int RvspId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool Attending { get; set; }
+        public string Accommondation { get; set; }
+        public int NumberOfNights { get; set; }
+        public string FoodInfo { get; set; }
+        public string Comment { get; set; }
+    }
+}
