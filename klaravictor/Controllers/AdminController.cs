e873340fc27a75e4d8f5b7121154aca9c59b81aa
Joakim Wågström <joakim.wagstrom@knowit.se>
2018-10-30 22:26:06 +0100

    Best admin login ever

--- klaravictor/Controllers/AdminController.cs
+++ klaravictor/Controllers/AdminController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Data.Entity;
+using System.Web;
+using System.Web.Mvc;
 using klaravictor.Models;
 using klaravictor.Services;
 
@@ -6,12 +9,39 @@ namespace klaravictor.Controllers
 {
     public class AdminController : Controller
     {
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            bool validUser = password == "password" && username == "user";
+
+            if (!validUser) return RedirectToAction("Index", "Page");
+
+            string authId = Guid.NewGuid().ToString();
+
+            Session["AuthID"] = authId;
+
+            var cookie = new HttpCookie("AuthID");
+            cookie.Value = authId;
+            Response.Cookies.Add(cookie);
+
+            return RedirectToAction("Index");
+        }
+
         public ActionResult Index()
         {
-            RvspService rvspService = new RvspService();
+            var requestCookie = Request.Cookies["AuthID"];
+            if (requestCookie != null && requestCookie.Value == Session["AuthID"].ToString())
+                {
+                    RvspService rvspService = new RvspService();
 
-            var rvsp = rvspService.GetRvsp();
-            return View(rvsp);
+                    var rvsp = rvspService.GetRvsp();
+                    return View(rvsp);
+                }
+
+                return View("Index");
         }
+
+
     }
 }
